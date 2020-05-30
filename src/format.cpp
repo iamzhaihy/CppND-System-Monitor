24da1f7259cf2fe5e531ee2c2007c8c1c542478a
iamzhaihy <harvey.zhai@gmail.com>
2020-05-30 15:54:04 -0400

    add formating functions

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,33 @@
-#include <string>
-
 #include "format.h"
 
-using std::string;
+#include <string>
 
-// TODO: Complete this helper function
+// stringify long with leading zero
+static std::string to_string(long i) {
+  std::string result;
+
+  if (i < 10) result += "0";
+
+  result += std::to_string(i);
+
+  return result;
+}
+
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+std::string Format::ElapsedTime(long seconds) {
+  long h, m;
+  h = m = 0;
+
+  while (seconds >= 3600) {
+    seconds -= 3600;
+    h++;
+  }
+
+  while (seconds >= 60) {
+    seconds -= 60;
+    m++;
+  }
+
+  return to_string(h) + ":" + to_string(m) + ":" + to_string(seconds);
+}
